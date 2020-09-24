0c12ddc413857d8829859055c20d169215eb9446
Dan Bobkov <39186942+Dan609@users.noreply.github.com>
2020-09-24 10:30:54 +0300

    Update traj_analysis_SB.R

--- traj_analysis_SB.R
+++ traj_analysis_SB.R
@@ -1,14 +1,15 @@
 # Cell movement trajectory analysis by Dan Bobkov, 2020 # dan.bobkov@gmail.com
 # Script desined to analyze tracks collected in ImageJ Manual tracking
 # plugin from CQ1 cytometer derived images, recorded during 4 h experiment
-# with Jurcat-HUVEC cells conditioned by co-cultivation and MbCD treatment.
-# Images were collected from SPIONs and conrtol wells, to model the classical
-# replicative senescense effects.
+# with Jurkat-HUVEC cells conditioned by co-cultivation and MbCD treatment.
+# Images were collected from MbCD treated and conrtol wells, to analyze the cells movement.
+##
 # If you use this script in your publications, please cite Bobkov D., Polyanskaya A., 
 # Musorina A., Lomert E., Shabelnikov S., Poljanskaya G. Replicative senescence in MSCWJ-1 
 # human umbilical cord mesenchymal stem cells is marked by characteristic changes in motility, 
 # cytoskeletal organization, and RhoA localization. Molecular Biology Reports. 2020. 47(5):3867-3883. 
 # doi: 10.1007/s11033-020-05476-6
+##
 # This script based on trajr package
 # If you use trajr in your publications, please cite McLean DJ, Skowron Volponi MA. trajr:
 # An R package for characterisation of animal trajectories. Ethology. 2018;12739.
